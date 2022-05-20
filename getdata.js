f846d18ff808f6fa777bb49c766a3a11eaf4f814
Adriana Oviedo <oviedolopezadriana13@gmail.com>
2022-05-19 23:05:25 -0400

    update 2

--- getdata.js
+++ getdata.js
@@ -6,10 +6,9 @@ const url = 'https://randomuser.me/api/'
 const getData = async () => {
 	try {
 		const { data } = await axios.get(url)
-		const { first, last } = data.results[0].name
 		const user = {
-			id: uuidv4().slice(30),
-			nombre: `${first} ${last}`,
+			id: uuidv4().slice(10),
+			nombre: `${ data.results[0].name.first} ${ data.results[0].name.last}`,
 			debe: 0,
 			recibe: 0,
 			correo: data.results[0].email,
